39718bc8b95be54f563e900cf1ce026d9f72dd17
Mau <mrdevs@outlook.es>
2021-11-18 18:55:19 -0600

    agregar la estructura y controlador inicial

---
+++ web/src/main/java/bananas/premium/web/controladores/indexController.java
@@ -0,0 +1,17 @@
+package bananas.premium.web.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inicio")
+public class indexController {
+    
+    @GetMapping({"/", "/index", "/inicio", "home"})
+    public String index(Model model){
+        return "/compras/index";
+    }
+
+}
