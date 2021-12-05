92efb524062c34d75a5f04593a824ae2f01189cb
Mau <mrdevs@outlook.es>
2021-12-04 18:28:23 -0600

    AGREGAR ARCHIVO INSERTS

---
+++ SQL_INSERTS.sql
@@ -0,0 +1,7 @@
+SELECT * FROM bananas_premium.helado;
+use bananas_premium;
+insert into helado(nombre, sabor,imagen,precio,existencia)values
+("Classic premium Lemon ice cream","Lemon","https://cdn1.cocina-familiar.com/recetas/helado-de-limon-casero-en-30-minutos.JPG",4.99,7),
+("Classic premium Strawberry ice cream","Strawberry","https://misrecetascocina.es/wp-content/uploads/2018/07/helado-fresa-thermomix-800x485.jpg",4.99,10),
+("Classic premium Oreo ice cream","OREO","https://i0.wp.com/www.lemonylimon.es/wp-content/uploads/2014/06/DSC_0046.jpeg?fit=1280%2C848&ssl=1",4.99,8),
+("Classic premium Chocolate ice cream","Chocolate","http://www.recetasya.com/wp-content/uploads/2012/07/helado-de-chocolate-casero.2jpg.jpg",4.99,3);
