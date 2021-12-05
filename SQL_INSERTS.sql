4759bc0fc330bd1edc4fe3cadfb013c3ca997587
Mau <mrdevs@outlook.es>
2021-12-04 21:49:56 -0600

    Actualizar inserts

--- SQL_INSERTS.sql
+++ SQL_INSERTS.sql
@@ -5,3 +5,12 @@ insert into helado(nombre, sabor,imagen,precio,existencia)values
 ("Classic premium Strawberry ice cream","Strawberry","https://misrecetascocina.es/wp-content/uploads/2018/07/helado-fresa-thermomix-800x485.jpg",4.99,10),
 ("Classic premium Oreo ice cream","OREO","https://i0.wp.com/www.lemonylimon.es/wp-content/uploads/2014/06/DSC_0046.jpeg?fit=1280%2C848&ssl=1",4.99,8),
 ("Classic premium Chocolate ice cream","Chocolate","http://www.recetasya.com/wp-content/uploads/2012/07/helado-de-chocolate-casero.2jpg.jpg",4.99,3);
+
+insert into usuario(nombre,contraseña,rol)values
+("banana-premium","4152","owner"),
+("banana-admin","1234","admin"),
+("banana-worker","4321","worker"),
+("banan-worker-2","6789","worker"),
+("banana-manager","9876","admin")
+;
+select * from usuario;
